d58eaac7547cc8b1cdbfd8b1f6776ac1b8fe2cc3
HavidDulsman <61195945+HavidDulsman@users.noreply.github.com>
2020-03-19 10:49:22 +0000

    Update run.sh

--- script/run.sh
+++ script/run.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 sudo systemctl cp /var/lib/jenkins/workspace/workout-pipline/script/flask.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable flask.service
