426fb68793bf90253c4922898312119a623f090f
HavidDulsman <david.hulsman@academytrainee.com>
2020-03-19 14:20:14 +0000

    potenial fix for pytest

--- script/test.sh
+++ script/test.sh
@@ -3,6 +3,6 @@
 source ~/.bashrc
 source venv/bin/activate
 python3 -m pytest ./test/test.py
-pip3 show coverage
-python3 -m coverage run -m pytest test/test.py
-python3 -m coverage report -m
+# pip3 show coverage
+coverage run -m pytest test/test.py
+coverage report 
